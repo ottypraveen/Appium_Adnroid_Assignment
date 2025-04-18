1188b6aadc8b40829cec89b4acdcf3df420c9d14
Otty Praveen <otty.praveen@gmail.com>
2025-04-18 12:49:31 +0530

    Android Appium Test

--- src/test/java/Appium/Appium_Assignment/BaseTest.java
+++ src/test/java/Appium/Appium_Assignment/BaseTest.java
@@ -1,5 +1,6 @@
 package Appium.Appium_Assignment;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
@@ -22,53 +23,64 @@ import com.google.common.collect.ImmutableMap;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.remote.AutomationName;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
 
 public class BaseTest {
-	
+
 	AndroidDriver driver;
 	UiAutomator2Options option;
-	
+
+	// public AppiumDriverLocalService service;
+
 	@BeforeTest
-	public void configureAppium() throws MalformedURLException, InterruptedException
-	{
-		 option=new UiAutomator2Options();
+	public void configureAppium() throws MalformedURLException, InterruptedException {
+		
+		/*
+		 * service=new AppiumServiceBuilder(). withAppiumJS(new File(
+		 * "C:\\Users\\ottyp\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"
+		 * )). withIPAddress("127.0.0.1"). usingPort(4723).build();
+		 */
+		
+		option = new UiAutomator2Options();
 		option.setDeviceName("PraveenEmulator2");
-		option.setApp("C:\\Users\\ottyp\\eclipse-workspace\\Appium_Assignment\\src\\test\\java\\Resources\\General-Store.apk");
+		option.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
+		option.setApp(
+				"C:\\Users\\ottyp\\eclipse-workspace\\Appium_Assignment\\src\\test\\java\\Resources\\General-Store.apk");
 		option.setChromedriverExecutable("C:\\Users\\ottyp\\eclipse-workspace\\Appium_Assignment\\chromedriver.exe");
-		 driver=new AndroidDriver(new URL("http://127.0.0.1:4723"), option);
-		 Thread.sleep(10000);
-		
-	}
-	
-	
-	  @AfterTest public void tearDown() { driver.quit(); }
-	 
-	
-	public void longPressaction(WebElement element)
-	{
-
-		JavascriptExecutor js=(JavascriptExecutor) driver;
-		js.executeScript("mobile: longClickGesture", ImmutableMap.of("elementId",  ((RemoteWebElement)element).getId(),"duration",2000));
-		
+		driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), option);
+		Thread.sleep(10000);
 
 	}
-	
-	
-	public Point getCenterofElement(Point location,Dimension size)
-	{
-		return new Point(location.getX()+size.getWidth()/2,
-				location.getY()+size.getHeight()/2);
+
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+		//service.stop();
 	}
-	
-	public void tapEvent(Point centerOfElement)
-	{
-		PointerInput Finger1 =new PointerInput(PointerInput.Kind.TOUCH,"Finger1");
-		  Sequence seq1=new Sequence(Finger1, 1).addAction(Finger1.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), centerOfElement))
-		  .addAction(Finger1.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
-		  .addAction(new Pause(Finger1, Duration.ofMillis(200)))
-		  .addAction(Finger1.createPointerMove(Duration.ofMillis(200), PointerInput.Origin.viewport(), centerOfElement))
-		  .addAction(Finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
-		  driver.perform(Collections.singletonList(seq1));
+
+	public void longPressaction(WebElement element) {
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("mobile: longClickGesture",
+				ImmutableMap.of("elementId", ((RemoteWebElement) element).getId(), "duration", 2000));
+
+	}
+
+	public Point getCenterofElement(Point location, Dimension size) {
+		return new Point(location.getX() + size.getWidth() / 2, location.getY() + size.getHeight() / 2);
+	}
+
+	public void tapEvent(Point centerOfElement) {
+		PointerInput Finger1 = new PointerInput(PointerInput.Kind.TOUCH, "Finger1");
+		Sequence seq1 = new Sequence(Finger1, 1)
+				.addAction(Finger1.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), centerOfElement))
+				.addAction(Finger1.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+				.addAction(new Pause(Finger1, Duration.ofMillis(200))).addAction(Finger1
+						.createPointerMove(Duration.ofMillis(200), PointerInput.Origin.viewport(), centerOfElement))
+				.addAction(Finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+		driver.perform(Collections.singletonList(seq1));
 	}
 
 }
